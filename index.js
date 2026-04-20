00bfe62571b1162af927b11d3a13c2c42f7ddfda
David Gili <d.gili.lopez@gmail.com>
2026-04-20 10:32:46 -0400

    operaciones registrar y leer funcionan correctamente

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { registrar, leer } = require("./operaciones.js");
 
+/*Capturamos los argumentos de la terminal en el orden ingresado con process.argv,
+se usa slice(2) porque los 2 primeros argumentos son "node" y el nombre del archivo.*/
+const [operacion, nombre, edad, animal, color, enfermedad] =
+  process.argv.slice(2);
+
+//Si el argumento capturado en "operacion" es "registrar" o "leer" ejecuta una u otra función
 if (operacion === "registrar") {
   registrar(nombre, edad, animal, color, enfermedad);
 }
--- operaciones.js
+++ operaciones.js
@@ -1,12 +1,20 @@
 const fs = require("fs");
 
-const registrar = (nombre, edad, animal, color, enfermedad) => {};
+const registrar = (nombre, edad, animal, color, enfermedad) => {
+  /*Primero leemos el contenido del archivo citas.json y lo parseamos para convertirlo en formato JSON
+      esto para agregar datos al archivo y no sobreescribir el contenido del archivo*/
+  const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+  //Luego le agregamos como objeto los argumentos recibidos en la función a través del push
+  citas.push({ nombre, edad, animal, color, enfermedad });
+  //Sobreescribimos el archivo citas.json con stringify para que el contenido tenga un string con formato JSON
+  fs.writeFileSync("citas.json", JSON.stringify(citas, null, 1));
+};
 
 const leer = () => {
-  const citas = fs.readFileSync("citas.json", "utf8");
-  JSON.parse(citas).forEach((cita) => {
-    console.log(cita);
-  });
+  //Leemos el archivo con parse para convertir el string con formato JSON a un arreglo con formato JSON
+  const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+  //Lo mostramos en consola
+  console.log(citas);
 };
 
 module.exports = { registrar, leer };
